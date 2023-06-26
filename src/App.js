50775817435527ceb6e1ac91ea5a517076b359ae
Sandesh Pandey <111729050+sandeshpandeycasual@users.noreply.github.com>
2023-06-26 09:48:48 +0000

    Update 22 files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { Box, Container, Typography } from "@mui/joy";
-import { useTimer } from "react-timer-hook";
-import { Navbar } from "./Navbar";
-import "./styles.css";
-import { useNavigate } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-  const time = new Date();
-  const navigate = useNavigate();
-  const expiryTimestamp = time.setSeconds(time.getSeconds() + 10);
-  const { seconds } = useTimer({
-    expiryTimestamp,
-    onExpire: () => navigate("/redirect")
-  });
+function App() {
   return (
-    <Box>
-      <Navbar />
-      <Container>
-        <Typography level="h3" sx={{ mt: 10 }}>
-          Due to low budget, we couldn't complete the app. So, for your
-          convenience we're redirecting you to Youtube in {seconds} seconds...{" "}
-        </Typography>
-      </Container>
-    </Box>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-import { CssVarsProvider, extendTheme } from "@mui/joy";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Router } from "../router";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-const theme = extendTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536
-    }
-  }
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <CssVarsProvider theme={theme}>
-      <Router />
-    </CssVarsProvider>
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
